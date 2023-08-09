eb6a5772c268c9955e873570250f9398df48f76b
Tom Tubbs <tomtubbs@mac.com>
2023-08-09 18:12:11 +0100

    onChange now deprecated in iOS 17

--- Instafilter/ContentView.swift
+++ Instafilter/ContentView.swift
@@ -8,13 +8,28 @@
 import SwiftUI
 
 struct ContentView: View {
-    var body: some View {
+	@State private var blurAmount = 0.0
+	
+	
+	
+	var body: some View {
         VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+			Text("🐻")
+				.font (Font.system(size: 120))
+				.blur(radius: blurAmount)
+				
+			
+			Slider(value: $blurAmount, in: 0...20)
+				.onChange(of: blurAmount) { newValue in
+					print("New value is \(newValue)")
+				}
+			Button("Apply Monday feeling") {
+				blurAmount = Double.random(in: 0...20)
+			}
         }
+		
+		
+		
         .padding()
     }
 }
